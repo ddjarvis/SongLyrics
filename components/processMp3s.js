8e5bf68415e973928ef5a00ad2526aa5cd2a6aa2
David Jarvis <iamdavidjarvis@gmail.com>
2026-05-20 12:24:05 +0800

    [2026-05-20 12:24:05] Moved checkTags to component: processMp3s.js

--- components/processMp3s.js
+++ components/processMp3s.js
@@ -3,3 +3,44 @@ import fs from 'node:fs/promises';
 import nodeid3lib from 'node-id3';
 const NodeID3 = nodeid3lib.Promise;
 
+
+async function checkTag(filepath) {
+		try {
+				// const tags = await NodeID3.read(filepath);
+				const fileBuf = await fs.readFile(filepath);
+				const tags = await NodeID3.read(fileBuf);
+			
+				// console.log(`Index:  ${idx}`);
+				let metaData = []
+				metaData.push(`Title:  ${tags.title || 'N/A'}`);
+				metaData.push(`Artist: ${tags.artist || 'N/A'}`);
+				metaData.push(`Album:  ${tags.album || 'N/A'}`);
+				metaData.push(`Album Artist:  ${tags.performerInfo || 'N/A'}`);
+				metaData.push(`Track:  ${tags.trackNumber || 'N/A'}`);
+				metaData.push(`Genre:  ${tags.genre || 'N/A'}`);
+				return metaData.join('\n');
+				// console.log(`\n`,tags);
+				
+				// Raw ID3v2 frames (like TIT2, TPE1) are stored inside tags.raw
+				// if (tags.raw) {
+					  // console.log(`Raw Title Frame (TIT2): ${tags.raw.TIT2}`);
+				// }
+				
+		} catch (error) {
+				console.error("Error reading ID3 tags:", error);
+		}
+}
+
+export default async function(mp3s) {
+	let i = 0;
+	for (let mp3 of mp3s) {
+		// console.time(`checkTag-${i}`);
+		i++;
+		let meta = await checkTag(mp3);
+		console.log(`--- Audio Metadata (${i}/${mp3s.length}) ---`);
+		console.log(meta);
+		console.log('--- ----- -- ----- ---');
+		console.log('');
+		// console.timeEnd(`checkTag-${i}`);
+	}
+}
--- index.js
+++ index.js
@@ -8,6 +8,8 @@ import { getInput, parseInput } from './components/getInput.js';
 import getDirectories from './components/getDirectories.js';
 import getMp3s from './components/getMp3s.js';
 import showHelp from './components/showHelp.js';
+import processMp3s from './components/processMp3s.js';
+
 const config = {};
 
 function displayHelp() {
@@ -60,47 +62,7 @@ const mp3list = getMp3s(dirs, config);
 
 
 
-async function checkTag(filepath, i) {
-		try {
-				const idx = ('0'.repeat(4)+i).substr(-4)
-				
-				// const tags = await NodeID3.read(filepath);
-				const fileBuf = await fs.readFile(filepath);
-				const tags = await NodeID3.read(fileBuf);
-			
-				console.log("--- Audio Metadata ---");
-				// console.log(`Index:  ${idx}`);
-				console.log(`Title:  ${tags.title || 'N/A'}`);
-				console.log(`Artist: ${tags.artist || 'N/A'}`);
-				console.log(`Album:  ${tags.album || 'N/A'}`);
-				console.log(`Album Artist:  ${tags.performerInfo || 'N/A'}`);
-				console.log(`Track:  ${tags.trackNumber || 'N/A'}`);
-				console.log(`Genre:  ${tags.genre || 'N/A'}`);
-				// console.log(`\n`,tags);
-				
-				// Raw ID3v2 frames (like TIT2, TPE1) are stored inside tags.raw
-				// if (tags.raw) {
-					  // console.log(`Raw Title Frame (TIT2): ${tags.raw.TIT2}`);
-				// }
-				console.log('--- ----- -- ----- ---');
-				console.log('');
-		} catch (error) {
-				console.error("Error reading ID3 tags:", error);
-		}
-}
 
-async function checkTags(mp3s) {
-	// console.time('checkTags');
-	for (let i in mp3s) {
-		// console.time(`checkTag-${i}`);
-		let mp3 = mp3s[i];
-		await checkTag(mp3, i);
-		// console.timeEnd(`checkTag-${i}`);
-	}
-	// console.timeEnd('checkTags');
-}
-
-
-checkTags(mp3list);
+processMp3s(mp3list);
 
 // time for i in {1..9}; do printf "[%d/9] %s" "${i}" "Loading..."; printf "\r\e[0J[%d/9] %s\n" "${i}" "$(g -r -e "*/.stversions/*" "#{SL2}")"; done
